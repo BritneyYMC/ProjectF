66c1fb08b8a800261cd27e861dbd7b14a5760eee
BritneyYMC <155485426+BritneyYMC@users.noreply.github.com>
2024-01-04 20:33:25 -0500

    Update DemoF.java

--- src/f/DemoF.java
+++ src/f/DemoF.java
@@ -7,4 +7,15 @@ public class DemoF {
 		System.out.println("Today is Thursday");
 		
 	}
+		//Coming from Github
+		int examOne = 85;
+		int examTwo = 90;
+		
+		if (examOne > examTwo) {
+			System.out.println("examOne score is higher");
+		} else if (examOne < examTwo){
+			System.out.println("examTwo score is higher");
+		} else if (examOne == examTwo){
+			System.out.println("Both exam score is same");
+		}
 }
